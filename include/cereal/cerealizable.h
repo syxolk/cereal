e7f9ff3bd696dd9825d21a109bef86c89ee58a69
Hans Kirchner <hans-kirchner@gmx.de>
2015-03-26 18:42:42 +0100

    Remove Cerealizable, Add macro

--- include/cereal/cerealizable.h
+++ include/cereal/cerealizable.h
@@ -4,24 +4,14 @@
 #include "lms/serializable.h"
 #include "cereal/archives/portable_binary.hpp"
 
-namespace cereal {
-
-template<class OArchive, class IArchive>
-class Cerealizable : public lms::Serializable {
-    virtual void serialize(std::ostream &os) const {
-        OArchive oarchive(os);
-        oarchive(this);
-    }
-
-    virtual void deserialize(std::istream &is) {
-        IArchive iarchive(is);
-        iarchive(this);
-    }
-};
-
-typedef Cerealizable<cereal::PortableBinaryOutputArchive,cereal::PortableBinaryInputArchive>
-    PortableBinaryCerealizable;
-
-}  // namespace cereal
+#define CEREAL_SERIALIZATION() \
+void serialize(std::ostream &os) const override { \
+	cereal::PortableBinaryOutputArchive oarchive(os); \
+	oarchive(*this); \
+} \
+void deserialize(std::istream &is) override { \
+	cereal::PortableBinaryInputArchive iarchive(is); \
+	iarchive(*this); \
+}
 
 #endif /* CEREAL_CEREALIZABLE_H */
